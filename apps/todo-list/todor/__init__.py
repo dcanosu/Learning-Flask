d2b9d472bd953826579ab355c87b7793fc194135
Dcanosu <dcanosu@gmail.com>
2025-07-04 09:15:34 -0500

    Adding unit5 - set up the login and sign up for users

--- apps/todo-list/todor/__init__.py
+++ apps/todo-list/todor/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     
@@ -19,5 +19,5 @@ def create_app():
     
     @app.route("/")
     def index():
-        return "Hello world !"
+        return render_template("index.html")
     return app
