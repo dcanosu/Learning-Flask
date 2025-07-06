9090e459b391990b47f3539c56705dd20fd80efe
Dcanosu <dcanosu@gmail.com>
2025-07-05 23:52:45 -0500

    Adding new views and the options to create new task with authentication

--- apps/todo-list/todor/todo.py
+++ apps/todo-list/todor/todo.py
@@ -1,11 +1,29 @@
-from flask import Blueprint
+import re
+from flask import Blueprint, render_template, request, redirect, url_for, g
+
+from todor.auth import login_required
+
+from .models import Todo, User
+from todor import db
 
 bp = Blueprint("todo", __name__, url_prefix="/todo")
 
 @bp.route("/list")
+@login_required
 def index():
-    return "List of task"
+    todos = Todo.query.all()
+    return render_template("todo/index.html", todos = todos)
 
-@bp.route("/create")
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
 def create():
-    return "Create a task"
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        
+        todo = Todo(g.user.id, title, desc)
+        
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for("todo.index"))
+    return render_template("todo/create.html")
